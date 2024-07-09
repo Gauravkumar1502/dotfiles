5e0276d5131e1711adbe773dda058e3cf3bd4b6e
Gauravkumar1502 <gauravkumar15021999@gmail.com>
2024-07-10 01:00:05 +0530

    Add Powershell Profile .ps1 script

---
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+# Setup Oh-My-Posh Theme
+
+# oh-my-posh init pwsh --config 'C:\Users\gaura\AppData\Local\Programs\oh-my-posh\themes\1_shell.omp.json' | Invoke-Expression
+# oh-my-posh init pwsh --config 'C:\Users\gaura\AppData\Local\Programs\oh-my-posh\themes\catppuccin_frappe.omp.json' | Invoke-Expression
+# oh-my-posh init pwsh --config 'C:\Users\gaura\AppData\Local\Programs\oh-my-posh\themes\cloud-context.omp.json' | Invoke-Expression
+# oh-my-posh init pwsh --config 'C:\Users\gaura\AppData\Local\Programs\oh-my-posh\themes\craver.omp.json' | Invoke-Expression
+# oh-my-posh init pwsh --config 'C:\Users\gaura\AppData\Local\Programs\oh-my-posh\themes\di4am0nd.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\gaura\AppData\Local\Programs\oh-my-posh\themes\negligible.omp.json' | Invoke-Expression
+
+# First Install the PSGallery Module for Terminal-Icons
+# Install-Module -Name Terminal-Icons -Repository PSGallery
+# Then Import the module
+Import-Module -Name Terminal-Icons
